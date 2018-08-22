2fcb82dc3922c8007709b77c75dd636fb1b6a1e7
DriscollRichie <driscollrichie@gmail.com>
2018-08-22 15:50:26 -0400

    Tracking login input

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Auth.css'
 import logo from '../../assets/auth_logo.png'
 
-export default function Auth() {
-  return (
-    <div id='auth-view'>
-      <section id='auth-login-mid'>
-        <div id='auth-login-content'>
-          <img src={logo} alt='logo' id='auth-logo' />
-          <div id='auth-input-section'>
-            <h3 className='auth-input-name'>Username</h3>
-            <input className='auth-input' />
-            <h3 className='auth-input-name'>Password</h3>
-            <input className='auth-input' />
+export default class Auth extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleChange(key, value) {
+    this.setState({[key]: value})
+  }
+
+  render() {
+    return (
+      <div id='auth-view'>
+        <section id='auth-login-mid'>
+          <div id='auth-login-content'>
+            <img src={logo} alt='logo' id='auth-logo' />
+            <div id='auth-input-section'>
+              <h3 className='auth-input-name'>Username</h3>
+              <input className='auth-input' onChange={e => this.handleChange('username', e.target.value)}/>
+              <h3 className='auth-input-name'>Password</h3>
+              <input className='auth-input' onChange={e => this.handleChange('password', e.target.value)}/>
+            </div>
+            <div id='auth-buttons'>
+              <button id='auth-login-button'>Login</button>
+              <button id='auth-register-button'>Register</button>
+            </div>
           </div>
-          <div id='auth-buttons'>
-            <button id='auth-login-button'>Login</button>
-            <button id='auth-register-button'>Register</button>
-          </div>
-        </div>
-      </section>
-    </div>
-  )
+        </section>
+      </div>
+    )
+  }
 }
