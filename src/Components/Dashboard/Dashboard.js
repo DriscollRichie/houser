45e3188a4e4dd4e9ed35476f9978087b193271b5
DriscollRichie <driscollrichie@gmail.com>
2018-08-31 14:49:01 -0400

    more dashboard styling

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -8,13 +8,22 @@ export default class Dashboard extends Component {
       <div id='dashboard-page'>
         <nav id='dashboard-nav'>
           <section id='dashboard-title'>
-            <img src={logo} width='25px'/>
+            <img src={logo} width='25px' />
             <h1 className='dashboard-nav-text'>Houser</h1>
             <p className='dashboard-nav-text'>Dashboard</p>
           </section>
           <p>Logout</p>
         </nav>
-        <section id='dashboard-mid'></section>
+        <section id='dashboard-mid'>
+          <button id='dashboard-add-propertyBtn'>Add new property</button>
+          <div id='dashboard-property-filter'>
+            <p>List properties with "desired rent" greater then: $</p>
+            <input id='dashboard-filter-input' placeholder='0'/>
+            <button className='dashboard-filter-controls'>Filter</button>
+            <button className='dashboard-filter-controls'>Reset</button>
+          </div>
+          <hr/>
+        </section>
       </div>
     )
   }
